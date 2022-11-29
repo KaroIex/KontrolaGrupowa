40ad384afb5963c1bb54a7078e0f5c8ca65f6e0c
Matek0611 <matiowo@wp.pl>
2022-11-29 17:09:35 +0100

    slownik PL->DE

---
+++ slownik_pl_de.py
@@ -0,0 +1,22 @@
+slownik_PL_DE = {
+    "cześć": "Hallo",
+    "jabłko": "Apfel",
+    "jajecznica": "Rührei",
+    "dzień": "Tag",
+    "noc": "Nacht",
+    "czas bożonarodzeniowy": "Weihnachtszeit",
+    "mleko": "Milch",
+    "kremówka": "Sahnekeks",
+    "woda": "Wasser",
+    "jeden": "eins",
+    "pierwszy": "ersten",
+    "kochać": "lieben",
+    "ja": "ich",
+    "ty": "du",
+    "on": "er",
+    "ona": "sie",
+    "ono": "es",
+    "my": "wir",
+    "wy": "ihr",
+    "oni": "sie"
+}
