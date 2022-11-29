60a58a3cf823cede67c844750b9b372de7c5b6c2
Your Name <you@example.com>
2022-11-29 16:16:10 +0100

    slownik polsko hiszpanski

---
+++ slownik_ang_pol.py
@@ -0,0 +1,24 @@
+slownik_POL_HISZP = {
+    "cześć": "Hallo",
+    "jabłko": "Apfel",
+    "jajecznica": "Rührei",
+    "dzień": "Tag",
+    "noc": "Nacht",
+    "czas bożonarodzeniowy": "Weihnachtszeit",
+    "mleko": "Milch",
+    "kremówka": "Sahnekeks",
+    "woda": "Wasser",
+    "jeden": "eins",
+    "pierwszy": "ersten",
+    "kochać": "lieben",
+    "ja": "ich",
+    "ty": "du",
+    "on": "er",
+    "ona": "sie",
+    "ono": "es",
+    "my": "wir",
+    "wy": "ihr",
+    "oni": "sie"
+}
+
+print(slownik_ANG_POL[input()])
