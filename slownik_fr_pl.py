67562de33d31033eb611fb2d312513abcedbada7
Matek0611 <matiowo@wp.pl>
2022-11-29 17:24:03 +0100

    slownik FR->PL

---
+++ slownik_fr_pl.py
@@ -0,0 +1,24 @@
+slownik_FR_PL = {
+    "Salut": "cześć",
+    "Pomme": "jabłko",
+    "Omelette": "jajecznica",
+    "Jour": "dzień",
+    "Nuit": "noc",
+    "Période de Noël": "czas bożonarodzeniowy",
+    "Lait": "mleko",
+    "Gâteau à la crème": "kremówka",
+    "Eau": "woda",
+    "un": "jeden",
+    "premier": "pierwszy",
+    "aimer": "kochać",
+    "moi": "ja",
+    "toi": "ty",
+    "lui": "on",
+    "elle": "ona",
+    "celui-ci": "ono",
+    "nous": "my",
+    "vous": "wy",
+    "eux": "oni"
+}
+
+print(slownik_FR_PL[input()])
