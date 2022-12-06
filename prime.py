c5ea5e1ccf0f57ddca0d66ea6ddb5359d7c30161
Sarneusz <mateuszgruszkiewicz@gmail.com>
2022-12-06 17:36:32 +0100

    prime

--- prime.py
+++ prime.py
@@ -2,4 +2,9 @@ class PrimeNumberFinder:
     def __init__(self, n):
         self.n = n
 
+def is_prime(self, num):
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
     
