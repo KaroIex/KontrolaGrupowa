a554c16f4dec2608d336571b7dab40e600a9a9a7
KaroIex <karolkula11@gmail.com>
2022-12-06 17:05:16 +0100

    dodanie pliku csv.py

---
+++ csv.py
@@ -0,0 +1,16 @@
+class CSVParser:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def parse_csv(self):
+        with open(self.filename, 'r') as file:
+            csv_reader = csv.reader(file)
+            data = list(csv_reader)
+            return data
+
+    def get_column(self, index):
+        data = self.parse_csv()
+        column = [row[index] for row in data]
+        return column
+
+    
