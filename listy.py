d588bca40f81b5b1c7e4eaad4ec977928d91dd8d
Marcin <matiowo@wp.pl>
2023-01-09 20:33:27 +0100

    zad 10 i 11 dodanie elementów do listy

--- listy.py
+++ listy.py
@@ -5,3 +5,5 @@ lista_lancuchow = sorted(lista_lancuchow)
 lista_lancuchow += ['kotek', 'ślimak', 'lustro', 'okno', 'yeti', 'jeż', 'mycie', 'koty', 'łupina', 'dwa']
 
 lista_lancuchow = sorted(lista_lancuchow, reverse=True)
+
+lista_lancuchow += ['kurczak', 'koń', 'lud', 'lodowiec', 'koszary', 'bagno', 'widok', 'telewizja', 'trzy', 'pięć']
