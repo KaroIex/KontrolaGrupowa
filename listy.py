5895a03de3e7a58014471a2224aecc11f1cf51c0
KaroIex <karolkula11@gmail.com>
2023-01-09 21:09:48 +0100

    dodanie 5 elementów

--- listy.py
+++ listy.py
@@ -57,3 +57,4 @@ lista_liczb = [1, 2, 3, 4, 5]
 lista_liczb.sort(reverse=True)
 lista_liczb.extend([6, 7, 8, 9, 10])
 lista_liczb.extend([11, 12, 13, 14, 15])
+lista_liczb.extend([16, 17, 18, 19, 20])
