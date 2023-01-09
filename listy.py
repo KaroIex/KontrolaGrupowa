19ea3f2a91bddd6fcedd5800aeaa62a9cce98d4d
Marcin <matiowo@wp.pl>
2023-01-09 20:31:06 +0100

    zad 9 sortowanie odwrotne

--- listy.py
+++ listy.py
@@ -3,3 +3,5 @@ lista_lancuchow = ['kaczka', 'dziwaczka', 'ananas', 'grzyb', 'kosa', 'kamień']
 lista_lancuchow = sorted(lista_lancuchow)
 
 lista_lancuchow += ['kotek', 'ślimak', 'lustro', 'okno', 'yeti', 'jeż', 'mycie', 'koty', 'łupina', 'dwa']
+
+lista_lancuchow = sorted(lista_lancuchow, reverse=True)
