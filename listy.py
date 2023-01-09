4b30ed0d3b66e679a774710f9a64bdb91223eb9f
your.user.name <mateuszgruszkiewicz@gmail.com>
2023-01-09 20:22:15 +0100

    listy zad5

---
+++ listy.py
@@ -0,0 +1,10 @@
+class Element:
+    def __init__(self, name):
+        self.name = name
+
+elements = []
+elements.append(Element("A"))
+elements.append(Element("B"))
+elements.append(Element("C"))
+elements.append(Element("D"))
+elements.append(Element("E"))
