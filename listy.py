eff6e594bbbdf81d875fc59320e4ea9bc3c1a589
your.user.name <mateuszgruszkiewicz@gmail.com>
2023-01-09 20:26:50 +0100

    listy zad9

--- listy.py
+++ listy.py
@@ -11,11 +11,18 @@ elements.append(Element("E"))
 
 elements.sort(key=lambda x: x.name)
 
+elements.append(Element("D"))
+elements.append(Element("U"))
+elements.append(Element("P"))
+elements.append(Element("K"))
+elements.append(Element("I"))
 
 elements.append(Element("K"))
 elements.append(Element("U"))
 elements.append(Element("R"))
 elements.append(Element("W"))
 elements.append(Element("Y"))
+
+
 
 elements.sort(key=lambda x: x.name, reverse=True)
